cc35f411cfd84a285dcb3dba8a0a3d39bde8b21d
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-02-10 07:43:18 +0300

    novie

---
+++ src/components/Movies.jsx
@@ -0,0 +1,15 @@
+// Обход и обработка полуенных данных фльмов
+import {Movie} from './Movie'
+
+function Movies (props) {
+    const {movies = []} = props
+
+    return <div className='movies'>
+        {movies.length ? movies.map(movie => ( 
+            <Movie key={movie.imdbId} {...movie}/>
+        )) : <h4>Nothing found...</h4>
+    }
+    </div>
+}
+
+export {Movies}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header () {
+        return <nav className="#1de9b6 teal accent-3">
+        <div className="nav-wrapper">
+        <a href="#" className="brand-logo">React Movies</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="!#">Repo</a></li>
+            
+        </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
